8c29143d70f0f9b17859d8336d6ca606bca3a1eb
Harry Nguyen <duongntse@gmail.com>
2020-08-21 17:42:07 +0700

    Updates

---
+++ precache-manifest.edc65d9de7880326bd1e22d2e2f68cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0282fa3d7fc0aca3ce87f861dd08f133",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8caadf09215a2fc4d2f4",
+    "url": "/my-app/static/css/main.2b0f36ae.chunk.css"
+  },
+  {
+    "revision": "9cf210f3422899028aad",
+    "url": "/my-app/static/js/2.cee930ec.chunk.js"
+  },
+  {
+    "revision": "7b98a553020ae68c17761985cb1987b4",
+    "url": "/my-app/static/js/2.cee930ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8caadf09215a2fc4d2f4",
+    "url": "/my-app/static/js/main.5f081b2b.chunk.js"
+  },
+  {
+    "revision": "287edd9be616fa5e5e74",
+    "url": "/my-app/static/js/runtime-main.8d44bb9f.js"
+  }
+]);
